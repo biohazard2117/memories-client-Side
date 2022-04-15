9e74a7b9e11d8e695ab4f4d2b4cdbc27750ae105
biohazard2117 <uttu.college.id@gmail.com>
2022-04-15 19:06:13 +0530

    create post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -4,9 +4,19 @@ export const getPosts = () => async (dispatch) =>{
     try {
         const {data} = await api.fecthPosts();
 
-        dispatch({type: 'FETCH_ALL', payload: []})
+        dispatch({type: 'FETCH_ALL', payload: data})
     } catch(err) {
         console.log(err.message);
     }
 
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+
+        dispatch({type: 'CREATE', payload: data});
+    } catch(err) {
+        console.log(err.message);
+    }
 }
